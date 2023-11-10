5884f10adc5a217411e51548d6b43dfa2e81017a
Rodrigo Pinto <digoamadeu@gmail.com>
2023-11-10 10:05:33 -0300

    finalizando projeto

--- src/js/index.js
+++ src/js/index.js
@@ -1,19 +1,1 @@
 import '../scss/styles.scss'
-import imageBuilding from '../assets/images/building.jpg'
-import imageNotebook from '../assets/images/notebook.jpg'
-
-
-const elemRoot = document.getElementById('root')
-elemRoot.classList.add('container')
-
-const elemImageBuilding = new Image()
-elemImageBuilding.src = imageBuilding
-
-const elemImageNotebook = new Image()
-elemImageNotebook.src = imageNotebook
-
-document.querySelector('#building')
-    .appendChild(elemImageBuilding)
-
-document.querySelector('#notebook')    
-    .appendChild(elemImageNotebook)
